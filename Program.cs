0abb6b3f861c030d23b9a2f8379238a148a56bae
gold <altintahsin0@gmail.com>
2026-05-04 07:24:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TahsinAltın_Odev1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+
+
+            }
+        }
+    }
+}
